cf11dda39a7056f5d9876f8b2e88b567c792f5e5
Tyler Hooton <2025hooton.tyler@fcboe.org>
2024-09-04 11:11:10 -0400

    EOC 9/4

---
+++ Part 1 - Formative/1.1 Caesar Shift/tests/test_caesar_encode.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestCaesarEncode(unittest.TestCase):
+    def test_caesar_encode_word_shifted_two(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
