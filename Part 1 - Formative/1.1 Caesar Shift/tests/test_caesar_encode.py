2d542f940082d3f038bd155dc8e65fecc3b62a8b
Tyler Hooton <2025hooton.tyler@fcboe.org>
2024-09-05 10:54:18 -0400

    caesar_encode unit testing

--- Part 1 - Formative/1.1 Caesar Shift/tests/test_caesar_encode.py
+++ Part 1 - Formative/1.1 Caesar Shift/tests/test_caesar_encode.py
@@ -1,10 +1,33 @@
 import unittest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from main import caesar_encode
+class MyTestCase(unittest.TestCase):
+    def test_caesar_encode_shifted_two(self):
+        self.assertEqual(caesar_encode("HELLOWORLD", 2), "JGNNQYQTNF")  # add assertion here
 
-class TestCaesarEncode(unittest.TestCase):
-    def test_caesar_encode_word_shifted_two(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_caesar_encode_shifted_zero(self):
+        self.assertEqual(caesar_encode("HELLOWORLD", 0), "HELLOWORLD")
 
+    def test_caesar_encode_shifted_negative_one(self):
+        self.assertEqual(caesar_encode("HELLOWORLD", -1), "GDKKNVNQKC")
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_caesar_encode_empty_string(self):
+        self.assertEqual(caesar_encode("", 1), "")
+
+    def test_caesar_encode_whitespace(self):
+        self.assertEqual(caesar_encode("  ", 1), "  ")
+
+    def test_caesar_encode_numbers(self):
+        self.assertEqual(caesar_encode("12", 2), "12")
+
+    def test_caesar_lower_case(self):
+        self.assertEqual(caesar_encode("helloworld", 2), "jgnnqytqtnf")
+
+    def test_caesar_punctuation(self):
+        self.assertEqual(caesar_encode("HELLOWORLD!", 2), "JGNNQYQTNF!")
+
+    def test_caesar_string_shift(self):
+        self.assertEqual(caesar_encode("HELLOWORLD", " "), "HELLOWORLD")
