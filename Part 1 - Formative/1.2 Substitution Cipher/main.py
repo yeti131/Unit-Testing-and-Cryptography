7b23e19bd2d59546a4027b717ed8ec08dc5b0dfe
Tyler Hooton <2025hooton.tyler@fcboe.org>
2024-09-09 10:53:43 -0400

    1.2 fixes, 1.3 complete

--- Part 1 - Formative/1.2 Substitution Cipher/main.py
+++ Part 1 - Formative/1.2 Substitution Cipher/main.py
@@ -1,23 +1,40 @@
 # Read the instructions to see what you need to do here!
 
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alpha_lower = alpha.lower()
 
 
 def sub_encode(text, codebet):
+    codebet = codebet.upper()
+    codebet_lower = codebet.lower()
     encoded_string = ""
     for i in range(len(text)):
         letter = text[i]
-        index = alpha.index(letter)
-        encoded_string += codebet[index]
+        if letter in alpha:
+            index = alpha.index(letter)
+            encoded_string += codebet[index]
+        elif letter in alpha_lower:
+            index = alpha_lower.index(letter)
+            encoded_string += codebet_lower[index]
+        else:
+            encoded_string += letter
     return encoded_string
 
 
 def sub_decode(text, codebet):
+    codebet = codebet.upper()
+    codebet_lower = codebet.lower()
     decoded_string = ""
     for i in range(len(text)):
         letter = text[i]
-        index = codebet.index(letter)
-        decoded_string += alpha[index]
+        if letter in alpha:
+            index = codebet.index(letter)
+            decoded_string += alpha[index]
+        elif letter in alpha_lower:
+            index = codebet_lower.index(letter)
+            decoded_string += alpha_lower[index]
+        else:
+            decoded_string += letter
     return decoded_string
 
 
