816b2e269125ad9ce9b8b86973f5de1b3a52e3c2
Tyler Hooton <2025hooton.tyler@fcboe.org>
2024-09-10 10:35:53 -0400

    vig_encode complete

--- Part 2 - Summative/2.1 Vigenere Cipher/main.py
+++ Part 2 - Summative/2.1 Vigenere Cipher/main.py
@@ -1,17 +1,31 @@
 # Read the instructions to see what you need to do here!
 
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alpha_lower = alpha.lower()
 
 def vig_encode(text, keyword):
     iterations = int((len(text) / len(keyword)) + 1)
-    key_string = keyword * iterations
+    key_string = (keyword * iterations).upper()
     encoded_string = ""
     for i in range(len(text)):
         letter = text[i]
-        text_index = alpha.index(letter)
-        key_letter = key_string[i]
-        key_index = alpha.index(key_letter)
-    return ""
+        if letter in alpha:
+            text_index = alpha.index(letter)
+            key_letter = key_string[i].upper()
+            key_index = alpha.index(key_letter)
+            code_index = (text_index + key_index) % 26
+            encoded_string += alpha[code_index]
+        elif letter in alpha_lower:
+            text_index = alpha_lower.index(letter)
+            key_letter = key_string[i].upper()
+            key_index = alpha.index(key_letter)
+            code_index = (text_index + key_index) % 26
+            encoded_string += alpha_lower[code_index]
+        else:
+            encoded_string += letter
+            continue
+
+    return encoded_string
 
 
 def vig_decode(text, keyword):
