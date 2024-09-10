8c37c56a0501b5d74820ead57ee012f524d5cf74
Tyler Hooton <2025hooton.tyler@fcboe.org>
2024-09-10 10:40:51 -0400

    vig_decode complete

--- Part 2 - Summative/2.1 Vigenere Cipher/main.py
+++ Part 2 - Summative/2.1 Vigenere Cipher/main.py
@@ -29,7 +29,28 @@ def vig_encode(text, keyword):
 
 
 def vig_decode(text, keyword):
-    return ""
+    iterations = int((len(text) / len(keyword)) + 1)
+    key_string = (keyword * iterations).upper()
+    decoded_string = ""
+    for i in range(len(text)):
+        letter = text[i]
+        if letter in alpha:
+            text_index = alpha.index(letter)
+            key_letter = key_string[i].upper()
+            key_index = alpha.index(key_letter)
+            code_index = (text_index - key_index) % 26
+            decoded_string += alpha[code_index]
+        elif letter in alpha_lower:
+            text_index = alpha_lower.index(letter)
+            key_letter = key_string[i].upper()
+            key_index = alpha.index(key_letter)
+            code_index = (text_index - key_index) % 26
+            decoded_string += alpha_lower[code_index]
+        else:
+            decoded_string += letter
+            continue
+
+    return decoded_string
 
 
 test = "THEQUICKBROWNFOXJUMPEDOVERTHELAZYDOG"
