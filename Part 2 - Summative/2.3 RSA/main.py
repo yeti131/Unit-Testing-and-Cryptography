92427dfb8849f772acd05d1b98447025ac1cda34
Tyler Hooton <2025hooton.tyler@fcboe.org>
2024-09-19 11:10:45 -0400

    EOC 9/19

--- Part 2 - Summative/2.3 RSA/main.py
+++ Part 2 - Summative/2.3 RSA/main.py
@@ -1,16 +1,47 @@
 import math
 
 # Copy and paste any functions you need from the Affine assignment!
+def mod_inverse_helper(a, b):
+  q, r = a // b, a % b
+  if r == 1:
+    return (1, -1 * q)
+  u, v = mod_inverse_helper(b, r)
+  return (v, -1 * q * v + u)
+
+
+def mod_inverse(a, m):
+  assert math.gcd(a, m) == 1, "You're trying to invert " + str(a) + " in mod " + str(m) + " and that doesn't work!"
+  return mod_inverse_helper(m, a)[1] % m
+
+alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+def convert_to_num(ngram):
+    """
+    Converts a ngram into a number.
+    :param ngram: ngram to be converted.
+    :return: Resultant number.
+    """
+    num = 0
+    count = 0
+    for letter in ngram:
+        num += 26 ** count * alpha.index(letter)
+        count += 1
+    return num
 
 # Also write these:
+
 def rsa_encode(text, m, e):
-  return 0
+    num = convert_to_num(text)
+    assert num < m
+    return 0
+
 
 def rsa_decode(num, m, d, l):
-  return ""
+    return ""
+
 
 def get_d(p, q, e):
-  return pow()
+    t = (p - 1) * (q - 1)
+    return mod_inverse(e, t)
 
 # text = "THEFIVEBOXINGWIZARDSJUMPQUICKLY"
 # l = len(text)
@@ -24,7 +55,7 @@ def get_d(p, q, e):
 # print(enc)
 # print(dec)
 # If this works, dec should be the same as text!
-
+print(get_d(2003, 2503, 17))
 
 # Part 2: Generate your own key!
 
